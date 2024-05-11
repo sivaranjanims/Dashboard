4f716089750c00e24c3c6155dc452cf360058dbe
sivaranjanims <141608041+sivaranjanims@users.noreply.github.com>
2024-05-11 12:45:39 +0530

    Add files via upload

---
+++ dashboard/script.js
@@ -0,0 +1,74 @@
+const body = document.querySelector("body");
+const darkLight = document.querySelector("#darkLight");
+const sidebar = document.querySelector(".sidebar");
+const submenuItems = document.querySelectorAll(".submenu_item");
+const sidebarOpen = document.querySelector("#sidebarOpen");
+const sidebarClose = document.querySelector(".collapse_sidebar");
+const sidebarExpand = document.querySelector(".expand_sidebar");
+sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+
+sidebarClose.addEventListener("click", () => {
+  sidebar.classList.add("close", "hoverable");
+});
+sidebarExpand.addEventListener("click", () => {
+  sidebar.classList.remove("close", "hoverable");
+});
+
+sidebar.addEventListener("mouseenter", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
+  }
+});
+sidebar.addEventListener("mouseleave", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+  }
+});
+
+darkLight.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    document.setI;
+    darkLight.classList.replace("bx-sun", "bx-moon");
+  } else {
+    darkLight.classList.replace("bx-moon", "bx-sun");
+  }
+});
+
+submenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("show_submenu");
+    submenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show_submenu");
+      }
+    });
+  });
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the dashboard content element
+  var dashboardContent = document.getElementById("dashboard-content");
+
+  // Get all the sidebar items
+  var sidebarItems = document.querySelectorAll(".item");
+
+  // Loop through each sidebar item and add click event listener
+  sidebarItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      // Get the text content of the clicked sidebar item
+      var textContent = this.querySelector(".navlink").textContent;
+
+      // Display the text content inside the dashboard
+      dashboardContent.textContent = textContent;
+    });
+  });
+});
